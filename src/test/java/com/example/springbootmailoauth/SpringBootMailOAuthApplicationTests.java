705452678b7c86d0b9ffb6105f0237acbd715596
minseong204 <minseong2083@icloud.com>
2024-04-19 19:57:17 +0900

    first commit

---
+++ src/test/java/com/example/springbootmailoauth/SpringBootMailOAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootmailoauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMailOAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
